72a9ac49277f5a534fdc39350bab87ad73920fe7
Munna <mehedihasanmunna516@gmail.com>
2026-01-29 20:29:51 +0600

    done

--- src/app/(pages)/refer/page.jsx
+++ src/app/(pages)/refer/page.jsx
@@ -1,5 +1,8 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import PreScreenWizard from '@/components/PreScreen/PreScreenWizard'
 
 export default function ReferPage() {
